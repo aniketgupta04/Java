69066fcdc60a70ca76ac81f33ee5bb6bac63f6ca
Aniket Gupta <cseews2318@glbitm.ac.in>
2025-05-15 00:27:18 +0530

    Hii

---
+++ Hello.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class notmyfirst{
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        System.out.println(s);
+
+        scanner.close();
+    }
+}
