9dbb69fc733ca83f5257e58894f2d93032f6ca35
Aniket Gupta <cseews2318@glbitm.ac.in>
2025-08-28 15:42:14 +0530

    Selection Sort

---
+++ selectionsort.java
@@ -0,0 +1,27 @@
+public class selectionsort {
+
+    void sort(int [] arr){
+        int n= arr.length;
+        for(int i=0;i<n-1;i++){
+            int min=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j]<arr[min]){
+                    min=j;
+                }
+            }
+                 int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+            }
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {50,95,8,7,45};
+        selectionsort ob = new selectionsort();
+        ob.sort(arr);
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
+    }
+
+}
